c60a7cf7f0961ff164f009439601a0e86755b627
kwakseobang <iii6602@naver.com>
2025-05-21 20:28:11 +0900

    Setting: 프로젝트 초기 셋팅

---
+++ src/test/java/com/kwakmunsu/likelionprojectteam1/LikelionProjectTeam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwakmunsu.likelionprojectteam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionProjectTeam1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
