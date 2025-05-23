863869746bbac8bf9d28bcf706e20e4f8d4c570e
kwakseobang <iii6602@naver.com>
2025-05-23 17:13:23 +0900

    Feat: LikeController Swagger 작성

--- src/main/java/com/kwakmunsu/likelionprojectteam1/domain/like/controller/LikeController.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/domain/like/controller/LikeController.java
@@ -2,14 +2,30 @@ package com.kwakmunsu.likelionprojectteam1.domain.like.controller;
 
 import com.kwakmunsu.likelionprojectteam1.domain.like.service.LikeCommandService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/like")
+@RequestMapping("/recipes")
 @RequiredArgsConstructor
 @RestController
 public class LikeController extends LikeDocsController {
 
     private final LikeCommandService likeCommandService;
+
+    @Override
+    @PostMapping("{recipeId}/likes")
+    public ResponseEntity<Void> like(@PathVariable(name = "recipeId") Long recipeId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    @DeleteMapping("{recipeId}/likes")
+    public ResponseEntity<Void> unLike(@PathVariable(name = "recipeId") Long recipeId) {
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/com/kwakmunsu/likelionprojectteam1/domain/like/controller/LikeDocsController.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/domain/like/controller/LikeDocsController.java
@@ -1,5 +1,68 @@
 package com.kwakmunsu.likelionprojectteam1.domain.like.controller;
 
+import com.kwakmunsu.likelionprojectteam1.global.exception.dto.response.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "Like Controller", description = "Like API")
 public abstract class LikeDocsController {
+
+    @Operation(
+            summary = "레시피 좋아요 등록 - JWT O",
+            description = """
+                     특정 레시피에 좋아요를 등록합니다. 이미 좋아요가 등록된 경우 409를 반환합니다.
+                    """
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "좋아요 등록 성공"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "존재하지 않은 레시피입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)
+                    )),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "이미 좋아요가 등록된 레시피입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)
+                    )),
+    })
+    public abstract ResponseEntity<Void> like(
+            @Parameter(description = "좋아요를 등록할 레시피의 ID", example = "123", required = true)
+            Long recipeId
+    );
+
+    @Operation(
+            summary = "레시피 좋아요 취소 - JWT O",
+            description = "특정 레시피에 등록된 좋아요를 취소합니다. 좋아요가 등록되어 있지 않으면 409를 반환합니다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "좋아요 취소 성공"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "존재하지 않은 레시피입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)
+                    )),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "좋아요가 등록되지 않은 레시피입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)
+                    )),
+    })
+    public abstract ResponseEntity<Void> unLike(
+            @Parameter(description = "좋아요를 취소할 레시피의 ID", example = "123", required = true)
+            Long recipeId
+    );
 
 }
