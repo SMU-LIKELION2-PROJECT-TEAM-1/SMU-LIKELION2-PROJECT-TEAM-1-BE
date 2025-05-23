548bfa2cc24c430dc23897200c5f286fbd0e9386
kwakseobang <iii6602@naver.com>
2025-05-23 17:12:49 +0900

    Feat: FavoritesController Swagger 작성

--- src/main/java/com/kwakmunsu/likelionprojectteam1/domain/favorites/controller/FavoritesController.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/domain/favorites/controller/FavoritesController.java
@@ -2,6 +2,10 @@ package com.kwakmunsu.likelionprojectteam1.domain.favorites.controller;
 
 import com.kwakmunsu.likelionprojectteam1.domain.favorites.service.FavoritesCommandService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class FavoritesController extends FavoritesDocsController {
 
     private final FavoritesCommandService favoritesCommandService;
+
+    @Override
+    @PostMapping("/{recipeId}")
+    public ResponseEntity<Void> favorites(@PathVariable(name = "recipeId") Long recipeId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    @DeleteMapping("/{recipeId}")
+    public ResponseEntity<Void> unFavorites(@PathVariable(name = "recipeId") Long recipeId) {
+        return ResponseEntity.noContent().build();
+    }
 
 }
