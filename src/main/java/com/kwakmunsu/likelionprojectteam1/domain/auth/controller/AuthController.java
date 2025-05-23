320c0fcf61118be9c9efe2ec2bdb268d09e68669
kwakseobang <iii6602@naver.com>
2025-05-23 17:10:47 +0900

    Feat: Auth Controller Swagger 작성

--- src/main/java/com/kwakmunsu/likelionprojectteam1/domain/auth/controller/AuthController.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/domain/auth/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.kwakmunsu.likelionprojectteam1.domain.auth.controller;
 
+import com.kwakmunsu.likelionprojectteam1.domain.auth.controller.dto.ReissueTokenRequest;
 import com.kwakmunsu.likelionprojectteam1.domain.auth.service.AuthCommandService;
 import com.kwakmunsu.likelionprojectteam1.domain.auth.service.AuthQueryService;
+import com.kwakmunsu.likelionprojectteam1.domain.auth.service.dto.TokenResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,16 @@ public class AuthController extends AuthDocsController {
 
     private final AuthCommandService authCommandService;
     private final AuthQueryService authQueryService;
+
+    @Override
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenResponse> reissue(@RequestBody ReissueTokenRequest request) {
+        TokenResponse response = TokenResponse.builder()
+                .accessToken("mock-access-token")
+                .refreshToken("mock-refresh-token")
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
 
 }
