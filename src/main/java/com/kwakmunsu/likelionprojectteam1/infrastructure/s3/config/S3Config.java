adcd552e9b20a771828dec450e218f7e9f419aad
곽문수 <iii6602@naver.com>
2025-05-26 17:28:15 +0900

    Feat- S3 이미지 업로드 기능 구현 (#17)

--- src/main/java/com/kwakmunsu/likelionprojectteam1/infrastructure/s3/config/S3Config.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/infrastructure/s3/config/S3Config.java
@@ -19,7 +19,6 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-
     @Bean
     public S3Client s3Client() {
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
