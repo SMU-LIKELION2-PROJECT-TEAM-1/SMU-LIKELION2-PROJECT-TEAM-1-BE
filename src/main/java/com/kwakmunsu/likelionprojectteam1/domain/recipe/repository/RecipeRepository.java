6659cfa799b0b1b5590711ef055774fab475b214
kwakseobang <iii6602@naver.com>
2025-05-26 17:19:20 +0900

    test: test용 recipe 생성 메서드

--- src/main/java/com/kwakmunsu/likelionprojectteam1/domain/recipe/repository/RecipeRepository.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/domain/recipe/repository/RecipeRepository.java
@@ -1,5 +1,6 @@
 package com.kwakmunsu.likelionprojectteam1.domain.recipe.repository;
 
+import com.kwakmunsu.likelionprojectteam1.domain.recipe.entity.Recipe;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,9 @@ import org.springframework.stereotype.Repository;
 public class RecipeRepository {
 
     private final RecipeJpaRepository recipeJpaRepository;
+
+    public Recipe create(Recipe recipe) {
+       return recipeJpaRepository.save(recipe);
+    }
 
 }
