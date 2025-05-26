e827f15995ba4b7f2dcc022d62b7bd6d230a3439
kwakseobang <iii6602@naver.com>
2025-05-26 17:18:47 +0900

    Feat: auto increment 기능 추가

--- src/main/java/com/kwakmunsu/likelionprojectteam1/domain/recipe/entity/Recipe.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/domain/recipe/entity/Recipe.java
@@ -1,14 +1,25 @@
 package com.kwakmunsu.likelionprojectteam1.domain.recipe.entity;
 
+import com.kwakmunsu.likelionprojectteam1.global.entity.BaseTimeEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 
 @Table(name = "recipe")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Recipe {
+public class Recipe extends BaseTimeEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
 
 }
