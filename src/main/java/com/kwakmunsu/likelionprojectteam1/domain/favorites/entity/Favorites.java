229fa78cd115b9db61cd6ba40390a4560f56a5fa
kwakseobang <iii6602@naver.com>
2025-05-28 21:27:04 +0900

    Feat: Favorites entity 필드 추가

--- src/main/java/com/kwakmunsu/likelionprojectteam1/domain/favorites/entity/Favorites.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/domain/favorites/entity/Favorites.java
@@ -1,14 +1,43 @@
 package com.kwakmunsu.likelionprojectteam1.domain.favorites.entity;
 
+import com.kwakmunsu.likelionprojectteam1.domain.member.entity.Member;
+import com.kwakmunsu.likelionprojectteam1.domain.recipe.entity.Recipe;
+import com.kwakmunsu.likelionprojectteam1.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "favorites")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
-public class Favorites {
+public class Favorites extends BaseTimeEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "recipe_id")
+    private Recipe recipe;
+
+    @Builder
+    private Favorites(Member member, Recipe recipe) {
+        this.member = member;
+        this.recipe = recipe;
+    }
 
 }
