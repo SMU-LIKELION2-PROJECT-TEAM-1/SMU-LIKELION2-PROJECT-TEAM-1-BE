8ff45d7e07dd0b6153da34c3ade7adc94cb7bebc
kwakseobang <iii6602@naver.com>
2025-05-28 21:33:28 +0900

    Fix: column에서 joinColumn으로 변경

--- src/main/java/com/kwakmunsu/likelionprojectteam1/domain/favorites/entity/Favorites.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/domain/favorites/entity/Favorites.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -27,11 +28,11 @@ public class Favorites extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @Column(name = "member_id")
+    @JoinColumn(name = "member_id")
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @Column(name = "recipe_id")
+    @JoinColumn(name = "recipe_id")
     private Recipe recipe;
 
     @Builder
--- src/main/java/com/kwakmunsu/likelionprojectteam1/domain/view/entity/View.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/domain/view/entity/View.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -17,8 +18,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Table(name = "view")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class View extends BaseTimeEntity {
 
@@ -27,11 +28,11 @@ public class View extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @Column(name = "member_id")
+    @JoinColumn(name = "member_id")
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @Column(name = "recipe_id")
+    @JoinColumn(name = "recipe_id")
     private Recipe recipe;
 
     @Builder
