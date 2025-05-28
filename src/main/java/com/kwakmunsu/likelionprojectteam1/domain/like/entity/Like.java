32a93040fc423decefd50ff0d1515547e29b153a
kwakseobang <iii6602@naver.com>
2025-05-28 21:26:51 +0900

    Feat: like entity 필드 추가

--- src/main/java/com/kwakmunsu/likelionprojectteam1/domain/like/entity/Like.java
+++ src/main/java/com/kwakmunsu/likelionprojectteam1/domain/like/entity/Like.java
@@ -1,14 +1,43 @@
 package com.kwakmunsu.likelionprojectteam1.domain.like.entity;
 
+import com.kwakmunsu.likelionprojectteam1.domain.member.entity.Member;
+import com.kwakmunsu.likelionprojectteam1.domain.recipe.entity.Recipe;
+import com.kwakmunsu.likelionprojectteam1.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "likes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
-public class Like {
+public class Like extends BaseTimeEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "recipe_id")
+    private Recipe recipe;
+
+    @Builder
+    private Like(Member member, Recipe recipe) {
+        this.member = member;
+        this.recipe = recipe;
+    }
 
 }
